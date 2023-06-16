09b9ae73cd4ee5f60ce898320201401725ea9a14
Your Name <you@example.com>
2023-06-16 19:50:28 +0600

    patch role set admin

--- index.js
+++ index.js
@@ -65,6 +65,21 @@ async function run() {
       res.send(Class);
     });
     
+    // set role
+    app.patch('/users/admin/:id', async (req, res) => {
+      const id = req.params.id;
+      console.log(id);
+      const filter = { _id: new ObjectId(id) };
+      const updateDoc = {
+        $set: {
+          role: 'admin'
+        },
+      };
+
+      const result = await usersCollection.updateOne(filter, updateDoc);
+      res.send(result);
+
+    })
     
 
     app.get('/users',async (req, res) => {
