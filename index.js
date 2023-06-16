b3f1fe3c6a4ce75750a0912f2031c7e06f8f4b1d
Your Name <you@example.com>
2023-06-16 20:20:12 +0600

    user delete bye id

--- index.js
+++ index.js
@@ -95,6 +95,15 @@ async function run() {
 
     })
 
+    // delete user
+
+    app.delete('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await usersCollection.deleteOne(query);
+      res.send(result);
+    })
+
     app.get('/users',async (req, res) => {
       const result = await usersCollection.find().toArray();
       res.send(result);
