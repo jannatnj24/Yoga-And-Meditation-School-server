322302b10166d3e967b7fa186a83b0e62eec00d7
Your Name <you@example.com>
2023-06-16 21:54:18 +0600

    jwt token veryfied

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express= require('express');
 const app =express();
+const jwt = require('jsonwebtoken');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors=require('cors');
 require('dotenv').config()
@@ -8,6 +9,26 @@ const port =process.env.PORT ||5000;
 
 app.use(cors());
 app.use(express.json());
+
+
+
+
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' });
+  }
+  // bearer token
+  const token = authorization.split(' ')[1];
+
+  jwt.verify(token, process.env.SECRET_JWT_TOKEN, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.kct9xpl.mongodb.net/?retryWrites=true&w=majority`;
